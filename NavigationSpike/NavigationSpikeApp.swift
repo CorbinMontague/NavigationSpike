737c02814097c3252b8b227078925df20acc5f4b
Corbin Montague <corbin@shiftkey.com>
2024-07-01 10:18:30 -0500

    Starting navigation spike

---
+++ NavigationSpike/NavigationSpikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSpikeApp.swift
+//  NavigationSpike
+//
+//  Created by Corbin Montague on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSpikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# NavigationSpike
+Spiking a homebrew solution leveraging native iOS 16+ SwiftUI Navigation APIs vs FlowStacks
